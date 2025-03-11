6af6b6911ccf009ce1eb9f85b7f1394556de81ea
Senadin Puce <senadin.puce@gmail.com>
2025-03-11 16:07:43 +0100

    Initial commit

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+
+namespace Mango.Services.CouponAPI
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			MapperConfiguration config = new(config =>
+			{
+				config.CreateMap<Coupon, CouponDto>();
+				config.CreateMap<CouponDto, Coupon>();
+			});
+			return config;
+		}
+	}
+}
